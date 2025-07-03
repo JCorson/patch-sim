27310b6d220ba3cd9ac3cf7b3f2daf17e4c12583
Jim Corson <jamesacorson@gmail.com>
2025-07-03 12:04:56 -0400

    REF: Move current and voltage clamp simulations to new module with separate tests

--- ap_sim/__init__.py
+++ ap_sim/__init__.py
@@ -4,11 +4,8 @@ Action Potential Simulator
 A package for simulating action potentials using the Hodgkin-Huxley model.
 """
 
-from .hodgkin_huxley import (
-    HodgkinHuxley,
-    simulate_voltage_clamp,
-    simulate_current_clamp,
-)
+from .hodgkin_huxley import HodgkinHuxley
+from .clamp_simulations import simulate_voltage_clamp, simulate_current_clamp
 from .nernst_neuron import nernst_potential
 from .utils import safe_exp, FloatOrArray
 
